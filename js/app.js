4bbdd51d72d7d528981064d1d784b659de9d4c8c
YoimelDev <yoimeldev@gmail.com>
2021-08-03 12:12:52 -0400

    fix js

--- js/app.js
+++ js/app.js
@@ -2,32 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     cargar();
 })
 
-function cargar() {    
-    const mostrar = document.querySelectorAll('.content__faq__box');
+function cargar() {
     const title = document.querySelectorAll('.content__faq__box__title');
         
-        mostrar.forEach((box) => {          
-            box.addEventListener('click', () => {  
-                removeMostrar();                
-                box.classList.add('mostrar');
-                agregarMostrar(box);                      
+    title.forEach((title) => {                         
+        title.addEventListener('click', () => {        
+                if (title.parentNode.classList.contains('mostrar')) {                    
+                    title.parentNode.classList.remove('mostrar');
+                } else {                    
+                    removeMostrar(); 
+                    title.parentNode.classList.add('mostrar');
+                }
             })    
         })
 
         function removeMostrar() {
-            mostrar.forEach(box => {
-                box.classList.remove('mostrar');  
+            title.forEach(mostrar => {
+                mostrar.parentNode.classList.remove('mostrar');  
             })
-        }
-
-        function agregarMostrar(box){
-            title.forEach((title) => {
-                title.parentNode.addEventListener('click', () => {
-                    title.parentNode.classList.toggle('mostrar');
-                })                    
-            })
-        }
-
-        
+        }        
 }
 
